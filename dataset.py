0846c5ad44f6d801d7872f4ee73d9cb1f046eea1
Woodrow Wang <woody@DN51ulb0.SUNet>
2019-05-15 00:21:10 -0700

    fix

---
+++ dataset.py
@@ -0,0 +1,74 @@
+from os.path import join
+import torch
+from torch.utils import data
+from config import *
+from load_data import preprocess
+import csv
+import numpy as np
+import random
+import cv2
+
+
+class Dataset(data.Dataset):
+    def __init__(self, data, augment_data=True, data_dir='data'):
+        self.data = data
+        self.augment_data = augment_data
+        self.data_dir = 'data'
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        h, w, c = CONFIG['input_height'], CONFIG['input_width'], CONFIG['input_channels']
+        #X = torch.zeros(shape=(h, w, c), dtype=torch.float32)
+        #y_steer = torch.zeros(shape=(1,), dtype=torch.float32)
+        #y_throttle = torch.zeros(shape=(1,), dtype=torch.float32) # UNUSED
+        ct_path, lt_path, rt_path, steer, throttle, brake, speed = self.data[index]
+
+        steer = np.float32(steer)
+        throttle = np.float32(throttle)
+        delta_correction = CONFIG['delta_correction']
+        camera = random.choice(['frontal', 'left', 'right'])
+        if camera == 'frontal':
+            frame = preprocess(cv2.imread(join(self.data_dir, ct_path.strip())))
+            steer = steer
+        elif camera == 'left':
+            frame = preprocess(cv2.imread(join(self.data_dir, lt_path.strip())))
+            steer = steer + delta_correction
+        elif camera == 'right':
+            frame = preprocess(cv2.imread(join(self.data_dir, rt_path.strip())))
+            steer = steer - delta_correction
+
+        if self.augment_data:
+
+            # mirror images with chance=0.5
+            if random.choice([True, False]):
+                frame = frame[:, ::-1, :]
+                steer *= -1.
+
+            # perturb slightly steering direction
+            steer += np.random.normal(loc=0, scale=CONFIG['augmentation_steer_sigma'])
+
+            # if color images, randomly change brightness
+            if CONFIG['input_channels'] == 3:
+                frame = cv2.cvtColor(frame, code=cv2.COLOR_BGR2HSV)
+                frame[:, :, 2] *= random.uniform(CONFIG['augmentation_value_min'], CONFIG['augmentation_value_max'])
+                frame[:, :, 2] = np.clip(frame[:, :, 2], a_min=0, a_max=255)
+                frame = cv2.cvtColor(frame, code=cv2.COLOR_HSV2BGR)
+        X = torch.as_tensor(frame)
+        y_steer = torch.as_tensor(steer)
+        return X, y_steer
+
+def main():
+    csv_driving_data = 'data/driving_log.csv'
+    with open(csv_driving_data, 'r') as f:
+        reader = csv.reader(f)
+        driving_data = [row for row in reader][1:]
+    data = Dataset(driving_data)
+    print(len(data))
+    X, y_steer = data[3]
+    print(X.shape, type(X))
+    print(y_steer.shape, type(y_steer))
+
+if __name__ == '__main__':
+    main()
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
 
 class NaiveConditionedCNN(nn.Module):
 	''' Simple conditioned CNN:
@@ -8,12 +9,77 @@ class NaiveConditionedCNN(nn.Module):
 	'''
 	def __init__(self):
 		super(NaiveConditionedCNN, self).__init__()
+		self.conv1 = Conv2dReluDropout(filter_size=5, pad=0, stride=2, num_filters=24, in_channels=3, dropprob=0.2)
+		self.conv2 = Conv2dReluDropout(filter_size=5, pad=0, stride=2, num_filters=36, in_channels=24, dropprob=0.2)
+		self.conv3 = Conv2dReluDropout(filter_size=5, pad=0, stride=2, num_filters=48, in_channels=36, dropprob=0.2)
+		self.conv4 = Conv2dReluDropout(filter_size=3, pad=0, stride=1, num_filters=64, in_channels=48, dropprob=0.2)
+		self.conv5 = Conv2dReluDropout(filter_size=3, pad=0, stride=1, num_filters=64, in_channels=64, dropprob=0.2)
+	
+		self.FC1 = FC(input_size=64 * 1 * 18, output_size = 100, dropprob=0.5) # See output of self.conv5
+		self.FC2 = FC(input_size=100, output_size=50, dropprob=0.5)
+		self.FC3 = nn.Linear(50, 10)
+		self.FC4 = nn.Linear(10, 1)
 
-	def forward(self, input) -> torch.Tensor:
+	def forward(self, x) -> torch.Tensor:
+		'''
+		param x: shape (N, 66, 200, 3) (N, H, W, C)
+		'''
 
+		N, H, W, C = x.shape
+		x = x.view(N, C, H, W)
+		# Skip normalization layer proposed in Bojarski et al. 
+		x = self.conv1(x)
+		x = self.conv2(x)
+		x = self.conv3(x)
+		x = self.conv4(x) # output shape (N, 64, 3, 20)
+		x = self.conv5(x) # output shape (N, 64, 1, 18)
 
+		# Flatten layer before FC layers
+		x = x.view(N, -1)
+
+		x = self.FC1(x)
+		x = self.FC2(x)
+		x = self.FC3(x)
+		x = F.relu(x)
+		x = self.FC4(x)
+		return x
+
+class FC(nn.Module):
+	def __init__(self, input_size, output_size, dropprob):
+		super(FC, self).__init__()
+		self.input_size = input_size
+		self.output_size = output_size
+		self.dropout = nn.Dropout(p=dropprob)
+		self.FC = nn.Linear(input_size, output_size)
+
+	def forward(self, x) -> torch.Tensor:
+		x_out = self.FC(x)
+		relu_x_out = F.relu(x_out)
+		out = self.dropout(relu_x_out)
+		return out
 
 class Conv2dReluDropout(nn.Module):
-	def __init__(self, filter_size, pad, stride, num_filters):
+	def __init__(self, filter_size, pad, stride, num_filters, in_channels, dropprob):
+		super(Conv2dReluDropout, self).__init__()
+		self.filter_size = filter_size
+		self.pad = pad
+		self.stride = stride
+		self.num_filters = num_filters
+		self.in_channels = in_channels
+		self.conv = nn.Conv2d(in_channels, num_filters, filter_size, stride, pad)
+		self.dropout = nn.Dropout(p=dropprob)
 
-	def forward(self, input) -> torch.Tensor:
+	def forward(self, x) -> torch.Tensor:
+		x_convout = self.conv(x)
+		relu_x_convout = F.relu(x_convout)
+		out = self.dropout(relu_x_convout)
+		return out
+
+def main():
+	data = torch.from_numpy(np.zeros((32, 66, 200, 3))).float()
+	model = NaiveConditionedCNN()
+	out = model(data)
+	print(out.shape)
+
+if __name__ == '__main__':
+	main()
